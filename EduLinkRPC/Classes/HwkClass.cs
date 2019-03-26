f442b682d8c1da6ecfe93252d6b20aff3df05126
CheAle14 <thedark1513@gmail.com>
2019-03-26 15:37:36 +0000

    Subject aliases

--- EduLinkRPC/Classes/HwkClass.cs
+++ EduLinkRPC/Classes/HwkClass.cs
@@ -11,13 +11,14 @@ namespace EduLinkRPC.Classes
     {
         public virtual string Name { get; set; }
         public virtual string Subject { get; }
+        public virtual List<string> SubjectAliases { get; set; }
         public virtual List<HwkUser> Users { get; set; } = new List<HwkUser>();
 
         public virtual bool SameSubject(Homework hwk)
         {
             var subject = hwk.Subject;
             subject = subject.Replace("Maths", "Mathematics");
-            return subject == Subject;
+            return subject == Subject || SubjectAliases.Contains(subject);
         }
     }
 }
